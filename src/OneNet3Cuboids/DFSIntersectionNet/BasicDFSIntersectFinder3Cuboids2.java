8d3cf1523b53acad8347b93e694e5903d61f0ef9
hidny <mtardibuono@gmail.com>
2023-01-24 22:54:11 -0500

    Went a bit further with search 3 cuboid intersect

--- src/OneNet3Cuboids/DFSIntersectionNet/BasicDFSIntersectFinder3Cuboids2.java
+++ src/OneNet3Cuboids/DFSIntersectionNet/BasicDFSIntersectFinder3Cuboids2.java
@@ -126,7 +126,7 @@ public class BasicDFSIntersectFinder3Cuboids2 {
 		CuboidToFoldOn cuboidsToBringAlongStartRot[] = new CuboidToFoldOn[2];
 		
 		int startIArea46 = 0;
-		int startJArea46 = 20; //(9 above bottom 10) I don't know the index...
+		int startJArea46 = 20; //(13 above bottom 10) I don't know the index...
 		
 		for(int i=0; i<startingPointsAndRotationsToCheck[0].size(); i++) {
 			
